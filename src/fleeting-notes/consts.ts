3202a6f942a19cddc442d6d087b0e6b67fc5443f
Artyom Prikhodko <homoogenecus@gmail.com>
2025-12-02 20:37:25 +0100

    extract DatePreset enum

--- src/fleeting-notes/consts.ts
+++ src/fleeting-notes/consts.ts
@@ -1,3 +1,10 @@
 export const FLEETING_NOTES_FOLDER = "FleetingNotes";
 export const FLEETING_NOTES_INBOX_FILE = "Inbox.md";
 export const FLEETING_NOTES_DATE_FORMAT = "yyyy-MM-dd";
+
+export enum DatePreset {
+	Today = "today",
+	Tomorrow = "tomorrow",
+	ThisWeek = "thisWeek",
+	Custom = "custom",
+}
