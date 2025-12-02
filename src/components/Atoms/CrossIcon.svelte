7dce4ae5ce40c7c4c72b1270b06412fd6a990727
Artyom Prikhodko <homoogenecus@gmail.com>
2025-12-02 20:36:59 +0100

    atoms: add CrossIcon component - simple wrapper for cross icon

---
+++ src/components/Atoms/CrossIcon.svelte
@@ -0,0 +1,32 @@
+<script lang="ts">
+	import { setIcon } from "obsidian";
+	import { onMount } from "svelte";
+
+	import Button from "./Button.svelte";
+
+	interface CrossIconProps {
+		buttonEl?: HTMLButtonElement | undefined;
+		size?: number;
+		className?: string;
+		ariaLabel?: string;
+		title?: string;
+		onClick?: () => void;
+	}
+
+	let {
+		buttonEl = $bindable(),
+		className = "",
+		ariaLabel = "Clear date button",
+		title = "",
+		onClick = () => {},
+	}: CrossIconProps = $props();
+
+	onMount(() => {
+		if (buttonEl) {
+			setIcon(buttonEl, "cross");
+		}
+	});
+</script>
+
+<Button bind:buttonEl variant="badge" {className} {ariaLabel} {onClick} {title}
+></Button>
