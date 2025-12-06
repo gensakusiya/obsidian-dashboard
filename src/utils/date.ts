494eff2b198eb378f77dedb43094601bbb8d2ba2
Artyom Prikhodko <homoogenecus@gmail.com>
2025-12-06 16:27:34 +0100

    add main function for date

--- src/utils/date.ts
+++ src/utils/date.ts
@@ -1,11 +1,12 @@
-import { format } from "date-fns";
+import { format, parse, isEqual } from "date-fns";
+import { DEFAULT_DATE_FORMAT } from "src/consts";
 
 export function formatDate(date: Date): string {
 	return format(date, "EEEE, MMMM d, yyyy");
 }
 
-export function formatDateToFleetingNote(date: Date): string {
-	return format(date, "yyyy-MM-dd");
+export function formatDateToDefault(date: Date): string {
+	return format(date, DEFAULT_DATE_FORMAT);
 }
 
 export function formatDateToDisplay(date: Date): string {
@@ -15,4 +16,25 @@ export function formatDateToDisplay(date: Date): string {
 export function parseDateFromString(dateStr: string): Date | null {
 	const parsedDate = new Date(dateStr);
 	return isNaN(parsedDate.getTime()) ? null : parsedDate;
+}
+
+export function isSameDate(
+	firstDate: string | Date,
+	secondDate: string | Date
+): boolean {
+	const lDate =
+		typeof firstDate === "string" ? parseFromString(firstDate) : firstDate;
+	const rDate =
+		typeof secondDate === "string"
+			? parseFromString(secondDate)
+			: secondDate;
+
+	return isEqual(lDate, rDate);
+}
+
+export function parseFromString(
+	date: string,
+	pattern: string = DEFAULT_DATE_FORMAT
+): Date {
+	return parse(date, pattern, new Date());
 }
