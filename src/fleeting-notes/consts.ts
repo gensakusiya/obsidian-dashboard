51447a3285b8108066542a4fcbf4333f597e37cf
Artyom Prikhodko <homoogenecus@gmail.com>
2025-12-06 16:28:32 +0100

    extract main date format pattern into global consts

--- src/fleeting-notes/consts.ts
+++ src/fleeting-notes/consts.ts
@@ -1,6 +1,6 @@
 export const FLEETING_NOTES_FOLDER = "FleetingNotes";
-export const FLEETING_NOTES_INBOX_FILE = "Inbox.md";
-export const FLEETING_NOTES_DATE_FORMAT = "yyyy-MM-dd";
+export const FLEETING_NOTES_DEFAULT_FILE = "Inbox";
+export const FLEETING_NOTES_INBOX_FILE = `${FLEETING_NOTES_DEFAULT_FILE}.md`;
 
 export enum DatePreset {
 	Today = "today",
