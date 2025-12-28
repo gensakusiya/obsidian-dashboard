3e9975d2e9fe4437863ed82a034bbdd118b22202
Artyom Prikhodko <homoogenecus@gmail.com>
2025-12-28 18:47:29 +0100

    atoms: add AddIcon button

---
+++ src/components/Atoms/AddIcon.svelte
@@ -0,0 +1,32 @@
+<script lang="ts">
+	import { setIcon } from "obsidian";
+	import { onMount } from "svelte";
+
+	import Button from "./Button.svelte";
+
+	interface AddIconProps {
+		buttonEl?: HTMLButtonElement | undefined;
+		size?: number;
+		className?: string;
+		ariaLabel?: string;
+		title?: string;
+		onClick?: () => void;
+	}
+
+	let {
+		buttonEl = $bindable(),
+		className = "",
+		ariaLabel = "Add button",
+		title = "",
+		onClick = () => {},
+	}: AddIconProps = $props();
+
+	onMount(() => {
+		if (buttonEl) {
+			setIcon(buttonEl, "plus");
+		}
+	});
+</script>
+
+<Button bind:buttonEl variant="badge" {className} {ariaLabel} {onClick} {title}
+></Button>
